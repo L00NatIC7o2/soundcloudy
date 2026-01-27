3ee49c846ae4b3754b17271cd9ed69e9c0e70464
L00NatIC7o2 <109448731+L00NatIC7o2@users.noreply.github.com>
2026-01-27 00:13:44 -0800

    Use SoundCloud connect endpoint for login

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,15 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const clientId = process.env.SOUNDCLOUD_CLIENT_ID;
+  const clientId = process.env.SOUNDCLOUD_CLIENT_ID!;
   const redirectUri = `${process.env.NEXTAUTH_URL}/api/auth/callback`;
   const scope = "non-expiring";
 
-  const authUrl = new URL("https://soundcloud.com/oauth2/authorize");
-  authUrl.searchParams.set("client_id", clientId!);
-  authUrl.searchParams.set("redirect_uri", redirectUri);
-  authUrl.searchParams.set("response_type", "code");
-  authUrl.searchParams.set("scope", scope);
+  const url = new URL("https://soundcloud.com/connect");
+  url.searchParams.set("client_id", clientId);
+  url.searchParams.set("redirect_uri", redirectUri);
+  url.searchParams.set("response_type", "code");
+  url.searchParams.set("scope", scope);
 
-  res.redirect(authUrl.toString());
+  res.redirect(url.toString());
 }
