b60ec847ded7215e75854cb08f15c74f3d616370
L00NatIC7o2 <109448731+L00NatIC7o2@users.noreply.github.com>
2026-01-26 18:21:36 -0800

    Recreate search route

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,7 +1,3 @@
-export const config = { runtime: "nodejs" };
-
-import axios from "axios";
-
 export default function handler(req, res) {
-  res.json({ ok: true, q: req.query.q || null, token: !!req.query.token });
+  res.json({ message: "search works" });
 }
