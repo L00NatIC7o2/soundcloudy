87db8385bf9ba9db6403b775b7c998c3924cef89
L00NatIC7o2 <109448731+L00NatIC7o2@users.noreply.github.com>
2026-01-27 17:21:31 -0800

    Fix global CSS import error and add login page styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
+import "../src/styles/main.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import "../src/styles/main.css";
 
 export default function Login() {
   const router = useRouter();
