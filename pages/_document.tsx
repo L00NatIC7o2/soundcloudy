4d5cd36506aba2c7fa7c19ed00133a19fe9f3522
L00NatIC7o2 <109448731+L00NatIC7o2@users.noreply.github.com>
2026-01-27 19:17:10 -0800

    Add mobile responsive design

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎵</text></svg>"
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
