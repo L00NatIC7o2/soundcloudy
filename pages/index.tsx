2e218ff93515becd3fb8eece4e5a8b38aa3d318c
L00NatIC7o2 <109448731+L00NatIC7o2@users.noreply.github.com>
2026-01-27 21:03:07 -0800

    Add useRef import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import Player from "../src/components/Player";
 
